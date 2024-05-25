f7eff452d8c4008194980681492ff0aa1b1ebbee
Ruslan Golovinskii <golxzn@gmail.com>
2024-05-25 10:20:26 +0400

    Fix linux build and add script to setup the env

---
+++ tools/linux/setup-environment.sh
@@ -0,0 +1,9 @@
+
+# Install PkgConfig
+sudo apt install pkg-config -y
+
+# Install xcb stuff
+sudo apt install xcb \
+	libxcb-keysyms1 libxcb-keysyms1-dev \
+	libxcb-xfixes0  libxcb-xfixes0-dev \
+	-y

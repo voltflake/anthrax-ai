02d7962b2cdd2909b6a54132c28d2bcc90f1c233
andrijzyn <62100293+andrijzyn@users.noreply.github.com>
2025-01-12 19:08:46 +0200

    [Tools] Added missing lib

--- tools/linux/install-vulkan-sdk.sh
+++ tools/linux/install-vulkan-sdk.sh
@@ -20,7 +20,7 @@ install_vulkan_pacman() {
     sudo pacman -Syu --noconfirm
 
     # Install Vulkan headers, tools, and drivers (for 64-bit systems)
-    sudo pacman -S --noconfirm vulkan-headers vulkan-tools
+    sudo pacman -S --noconfirm vulkan-headers vulkan-tools vulkan-utility-libraries
 
     # Install GPU-specific Vulkan drivers (for example, for Intel or AMD)
     if lspci | grep -i "vga" | grep -i "amd"; then
